b9a05efb5a5f99695b8ec0f5c15379239cbb914d
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-04-09 19:22:36 -0300

    feat: add component Logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import { ContentLogo } from "./LogoStyle"
+
+const Logo = () => {
+  return(
+    <a><ContentLogo src="/logotopfloors.png" alt="Logo Principal" /></a>
+  )
+}
+
+export default Logo
---
+++ src/components/LogoStyle.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ContentLogo = styled.img`
+  width: 156px;
+  cursor: pointer;
+`
