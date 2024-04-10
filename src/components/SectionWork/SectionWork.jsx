2141bf587e1bd4ed3a0c70cf73b288a106e0a925
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-04-10 14:32:40 -0300

    feat: add component Section Work

---
+++ src/components/SectionWork/SectionWork.jsx
@@ -0,0 +1,54 @@
+import Paragraph from "../Paragraph"
+import SecondTitle from "../SecondTitle"
+import { ContainerSliderWork, ContainerWork, ContentSliderWork, ContentTextWork } from "./SectionWorkStyle"
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+
+const SectionWork = () => {
+  
+  const data = [
+    {id: '1', img: '/slider-work1.png' },
+    {id: '2', img: '/slider-work2.png' },
+    {id: '3', img: '/slider-work3.png' },
+    {id: '4', img: '/slider-work4.png' },
+    {id: '5', img: '/slider-work5.png' },
+    {id: '6', img: '/slider-work6.png' },
+
+  ] 
+
+  return(
+    <ContainerWork>
+      <ContentTextWork>
+        <SecondTitle textSecondTitle='What we love to do'/>
+        <Paragraph textParagraph='We install unfinished and prefinished hardwoods, luxury vinyl Plank (LVP), luxury vinyl tile (LVT), laminate, and engineered floors. In addition, we refinish all types of hardwood floors. Our expert team is also specialized in installing and refinishing staircases.'/>
+      </ContentTextWork>
+      <ContainerSliderWork>
+      <Swiper
+        loop={true}
+        slidesPerView={2}
+        pagination={true}
+        centeredSlides={false}
+        spaceBetween={30}
+        autoplay={{
+          delay: 3800,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Pagination ]}
+        className="mySwiper"
+      >
+      {data.map((slider) => (
+        <SwiperSlide key={slider.id}>
+          <ContentSliderWork>
+            <img src={slider.img} alt="Image Banner Work" />
+          </ContentSliderWork>
+        </SwiperSlide>
+      ))}
+      </Swiper>
+      </ContainerSliderWork>
+    </ContainerWork>
+  )
+}
+
+
+export default SectionWork
---
+++ src/components/SectionWork/SectionWorkStyle.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const ContainerWork = styled.section`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 10rem 1rem 5rem;
+
+
+
+`
+
+export const ContentTextWork = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`
+export const ContainerSliderWork = styled.div`
+  width: 100%;
+`
+export const ContentSliderWork = styled.div`
+  width: 100%;
+  img{ 
+    width: 100%;
+    cursor: pointer;
+  }
+`
