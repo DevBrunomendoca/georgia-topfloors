b37090caceddbd769b87ab24cd6c19954750860a
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-04-10 11:01:42 -0300

    feat: add component Paragraph

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,9 @@
+import { ContentParagraph } from "./ParagraphStyle"
+
+const Paragraph = ({textParagraph}) => {
+  return(
+    <ContentParagraph>{textParagraph}</ContentParagraph>
+  )
+}
+
+export default Paragraph
---
+++ src/components/ParagraphStyle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContentParagraph = styled.p`
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  font-size: 1.8rem;
+  max-width: 1060px;
+  text-align: center;
+  color: var(--third-color);
+`
