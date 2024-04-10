f8d99aa27293140c9c7e4ad596cd8ebc7be69de0
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-04-10 11:03:32 -0300

    feat: add component SecondTitle

---
+++ src/components/SecondTitle.jsx
@@ -0,0 +1,9 @@
+import { ContentSecondTitle } from "./SecondTitleStyle"
+
+const SecondTitle = ({textSecondTitle}) => {
+  return(
+    <ContentSecondTitle>{textSecondTitle}</ContentSecondTitle>
+  )
+}
+
+export default SecondTitle
---
+++ src/components/SecondTitleStyle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContentSecondTitle = styled.h2`
+  font-family: "Playfair Display", serif;
+  color: var(--third-color);
+  font-size: 3.8rem;
+  font-weight: 600;
+  text-align:center;
+  
+`
