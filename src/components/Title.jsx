31377733ccd06dadaa85240c3ae26ea210f61b4f
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-04-10 11:00:55 -0300

    feat: add component Title

---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+import { ContentTitle } from "./TitleStyle"
+
+const Title = ({textTitle}) => {
+  return(
+    <ContentTitle>{textTitle}</ContentTitle>
+  )
+}
+
+export default Title
---
+++ src/components/TitleStyle.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const ContentTitle = styled.h1`
+  font-family: "Playfair Display", serif;
+  color: var(--third-color);
+  font-size: 6.6rem;
+  font-weight: 600;
+  max-width: 1060px;
+  text-align:center;
+  
+`
